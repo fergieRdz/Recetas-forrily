97c0d83d70e36348d64407dd0f237eaa0eeda007
Fernanda Rodriguez <ma_fera_22@hotmail.com>
2026-02-02 00:28:32 -0600

    added config.js

--- src/js/model.js
+++ src/js/model.js
@@ -1,3 +1,5 @@
+import { API_URL } from './config.js';
+
 export const state = {
   recipe: {},
 };
@@ -6,7 +8,7 @@ export const loadRecipe = async function (id) {
   try {
     if(!id) return;
 
-    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+    const res = await fetch(`${API_URL}${id}`);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
