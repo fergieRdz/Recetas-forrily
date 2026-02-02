58e994b185e1928609ef5433e3b9ce045f954805
Fernanda Rodriguez <ma_fera_22@hotmail.com>
2026-02-02 01:12:27 -0600

    limit results to 10 at once

--- src/js/controller.js
+++ src/js/controller.js
@@ -22,7 +22,7 @@ const controlSearchResults = async function () {
     
     await model.loadSearchResults(query);
     
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
