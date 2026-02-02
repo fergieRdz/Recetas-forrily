5bac293d69dc486d741f78842425f4ba37378c9c
Fernanda Rodriguez <ma_fera_22@hotmail.com>
2026-02-02 01:07:16 -0600

    added error handler for list display

--- src/js/views/ResultsView.js
+++ src/js/views/ResultsView.js
@@ -3,6 +3,8 @@ import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query';
+  _message = '';
 
   _generateMarkup() {
     return this._data.map(this._generateMarkupPreview).join('');
